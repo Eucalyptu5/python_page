9d2ed4a32e07b2628cbc3ab7085c60fdb798c86b
Eucalyptu5 <info@nbrandtner.com>
2025-05-01 23:22:41 +0200

    Update python-lerndoku.py

--- python-lerndoku.py
+++ python-lerndoku.py
@@ -17,3 +17,19 @@ Als Beispiel kann man mit der Funktion print die Variable sich einfach ausgeben 
 """
 print(x)
 
+"""
+2. Einfache Datentypen
+3. Strings
+4. integer & float
+5. Boolean & if / else
+6. Listen in Python
+7. Tupel
+8. While Schleife
+9. For Schleife
+10. Disctionaries
+11. Funktionen
+12. Exceptions
+13. Module
+14. OOP (Objektorientierte Programmierung)
+15. Machine Learning
+"""
