a14e1c53fcb62d304815c7579edfafcf278dd4bd
Eucalyptu5 <info@nbrandtner.com>
2025-05-01 22:58:05 +0200

    Add files via upload
    Variablen Doku GitHub

---
+++ variablen.py
@@ -0,0 +1,15 @@
+"""
+Dies ist das Skript, um alles gelernte über Variablen in Paython auf GitHub zu dokumentieren
+"""
+
+# Eine erste Variable definieren. In diesem Fall haben wir die Variable x als eine ganze Zahl (int) definiert.
+
+x = 50
+
+""" 
+Nachdem man eine Variable definiert hat kann man diese im späteren Verlauf des Skipts wieder verwenden. 
+Python geht das Skript immer von oben nach unten durch, daher ist es notwendig die Variable im Skript zu definieren, bevor man diese wieder im Skript verwendet
+Als Beispiel kann man mit der Funktion print die Variable sich einfach ausgeben lassen. 
+"""
+print(x)
+
