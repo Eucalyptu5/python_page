72c078af2f3d8dfc7906ed9493ea600379b3fe49
Eucalyptu5 <info@nbrandtner.com>
2025-05-01 23:13:55 +0200

    Update and rename variablen.py to python-lerndoku.py

--- python-lerndoku.py
+++ python-lerndoku.py
@@ -1,19 +1,16 @@
-"""
-Dies ist das Skript, um alles gelernte über Variablen in Paython auf GitHub zu dokumentieren
-
-Variablen sind PLatzhalter, dem Werte zugewiesen werden können
-zur Wiederverwendung an späterer Stelle im Programm
-x = 50
-
-print("Die Variable hat den Wert" , x ,)
+# Dies ist das Skript, um alles gelernte über Python auf GitHub zu dokumentieren
 
 """
+1. Variablen:
+- Variablen sind Platzhalter, dem Werte zugewiesen werden können
+- Zur Wiederverwendung an späterer Stelle im Programm
+""" 
 
 # Eine erste Variable definieren. In diesem Fall haben wir die Variable x als eine ganze Zahl (int) definiert.
 
 x = 50
 
-""" 
+"""
 Nachdem man eine Variable definiert hat kann man diese im späteren Verlauf des Skipts wieder verwenden. 
 Python geht das Skript immer von oben nach unten durch, daher ist es notwendig die Variable im Skript zu definieren, bevor man diese wieder im Skript verwendet
 Als Beispiel kann man mit der Funktion print die Variable sich einfach ausgeben lassen. 
